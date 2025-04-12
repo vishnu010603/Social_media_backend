6a2e3d3975f9ad56bdd8bafef1e4fa377e3b8681
vishnu <k.vishnuvardhan0106@gmail.com>
2025-04-12 23:45:04 +0530

    Intial commit project setup done

---
+++ constants.js
@@ -0,0 +1,2 @@
+export const DB_NAME = "social-media-app";
+
---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express, { urlencoded } from 'express';
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+app.use('/ping', (req, res) => {
+    res.send('pong');
+});
+
+
+
+export { app } 

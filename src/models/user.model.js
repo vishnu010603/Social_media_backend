e7730a54ffa833d92d335bec65d815935a46fa92
vishnu <k.vishnuvardhan0106@gmail.com>
2025-04-13 00:02:14 +0530

    Added user model

---
+++ src/models/user.model.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePic: { type: String, default: "" },
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);

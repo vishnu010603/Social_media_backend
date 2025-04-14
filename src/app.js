6eb253e9f2973679a5130c642a3d07fa6a5f2e04
vishnu <k.vishnuvardhan0106@gmail.com>
2025-04-14 13:04:52 +0530

    Added .env file

--- src/app.js
+++ src/app.js
@@ -26,12 +26,7 @@ app.use('/api/comments', commentRoutes);
 
 // Root route
 app.get('/', (req, res) => res.send('API is running'));
-const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
-const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
-const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+
 export {
-    CLOUDINARY_CLOUD_NAME,
-    CLOUDINARY_API_KEY,
-    CLOUDINARY_API_SECRET,
     app
 }
